a54b35433c5c1b966ec88a4a0b631433b3aac7fb
Urien Nolasco <urien.dragon@gmail.com>
2026-06-17 20:02:35 -0300

    style(search): adiciona chaves em if do datasource

--- lib/features/search/data/datasources/search_remote_datasource.dart
+++ lib/features/search/data/datasources/search_remote_datasource.dart
@@ -53,8 +53,9 @@ class SearchRemoteDataSource {
       if (data == null) throw const UnknownApiException();
 
       List<Map<String, dynamic>> readList(dynamic d) {
-        if (d is List<dynamic>)
+        if (d is List<dynamic>) {
           return d.whereType<Map<String, dynamic>>().toList();
+        }
         if (d is Map<String, dynamic>) {
           for (final key in const [
             'data',
