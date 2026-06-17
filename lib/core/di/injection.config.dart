aab6d80fe426dec624defab62b404720cd7aa6a8
Henrique Bassanesi Martins <henriquebassanesi@gmail.com>
2026-06-17 00:31:57 -0300

    fix: remove ConfirmProducerDeliveryWithCode from DI factory matching constructor signature

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -201,8 +201,6 @@ import 'package:ragro_mobile/features/producer_orders/data/repositories/route_re
     as _i609;
 import 'package:ragro_mobile/features/producer_orders/domain/repositories/producer_orders_repository.dart'
     as _i649;
-import 'package:ragro_mobile/features/producer_orders/domain/usecases/confirm_producer_delivery_with_code.dart'
-    as _i474;
 import 'package:ragro_mobile/features/producer_orders/domain/usecases/confirm_producer_order.dart'
     as _i141;
 import 'package:ragro_mobile/features/producer_orders/domain/usecases/get_producer_order_detail.dart'
@@ -597,11 +595,6 @@ extension GetItInjectableX on _i174.GetIt {
         gh<_i671.ActivateAdminProducer>(),
       ),
     );
-    gh.lazySingleton<_i474.ConfirmProducerDeliveryWithCode>(
-      () => _i474.ConfirmProducerDeliveryWithCode(
-        gh<_i649.ProducerOrdersRepository>(),
-      ),
-    );
     gh.lazySingleton<_i141.ConfirmProducerOrder>(
       () => _i141.ConfirmProducerOrder(gh<_i649.ProducerOrdersRepository>()),
     );
@@ -692,17 +685,16 @@ extension GetItInjectableX on _i174.GetIt {
         gh<_i774.ConfirmCustomerDelivery>(),
       ),
     );
-    gh.factory<_i432.RateProducerBloc>(
-      () => _i432.RateProducerBloc(gh<_i5.CreateReview>()),
-    );
     gh.factory<_i921.ProducerOrderDetailBloc>(
       () => _i921.ProducerOrderDetailBloc(
         gh<_i181.GetProducerOrderDetail>(),
         gh<_i141.ConfirmProducerOrder>(),
         gh<_i885.RefuseProducerOrder>(),
         gh<_i1038.UpdateProducerOrderStatus>(),
-        gh<_i474.ConfirmProducerDeliveryWithCode>(),
       ),
+    );
+    gh.factory<_i432.RateProducerBloc>(
+      () => _i432.RateProducerBloc(gh<_i5.CreateReview>()),
     );
     return this;
   }
